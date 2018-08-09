085f2058c8c6ac8fac34e08a3a5bfc3c9ebabe02
Sawyer2016 <szhou4@student.unimelb.edu.au>
2018-08-09 17:19:22 +0800

    继承

--- js技巧/继承.js
+++ js技巧/继承.js
@@ -1,12 +1,17 @@
+//每个构造函数都有prototype
+//每个对象的__proto__属性指向自身构造函数的prototype
+
 //原型链继承,父类的属性会变成公有属性,子类的原型对象的构造函数是父类构造函数
 //存在的问题：对于父类中引用类型的属性，在一个实例里面修改，其他实例里的属性也会修改
 function Son(){}
 function Father(){}
 Son.prototype = new Father()
+Son.prototype.constructor=Son     //子类实例的构造函数变为Son
 
-// 借用构造函数,为了解决原型链继承的问题
+// 构造函数继承,为了解决原型链继承的问题
 // 在子类型构造函数内部调用超类型构造函数，可以传递参数(没有用到原型链)
-// 问题：父类的prototype里面定义的属性和方法在子类中无法使用
+// 问题：父类的prototype里面定义的属性和方法在子类中无法使用。通常会把方法定义在prototype，
+// 这样每次实例化不用再创建一遍方法，但是这样会造成子类无法复用父类方法
 function SuperType(){
     this.colors = ["red", "blue", "green"];
 }
@@ -21,7 +26,7 @@ var instance2 = new SubType();
 alert(instance2.colors);    //"red,blue,green"
 
 
-// 组合继承(组合原型链和借用构造函数) 解决借用构造函数的问题
+// 组合继承(组合原型链和构造函数) 解决借用构造函数的问题
 // 父类的属性相关的函数就赋给父类原型，如果是子类属性就赋给子类原型
 //问题：会调用两次超类型构造函数（一次是在创建子类型，一次是在子类型内调用父类构造函数）
 function SuperType(name){
@@ -60,14 +65,19 @@ instance2.sayAge();
 
 
 //原型式继承
+//问题：父类的引用属性会被所有子类共享
 function object(o){
     function F(){}
     F.prototype=o
     return new F()
 }
 
+//该函数等同于 Object.create(obj)
+
+
 
 //寄生式继承
+//增加了方法
 function createAnother(original){ 
     var clone=object(original); 
     clone.sayHi=function(){
@@ -83,9 +93,7 @@ function createAnother(original){
 //     subType.prototype = prototype;
 // }
 function inheritPrototype(subType, superType){
-    function F(){}
-    F.prototype=superType.prototype
-    var proto=new F()
+    let proto=Object.create(superType.prototype)
     proto.constructor=subType
     subType.prototype = proto
 
@@ -101,7 +109,24 @@ function SubType(name, age){
     SuperType.call(this, name);
     this.age = age;
 }
-inheritPrototype(SubType, SuperType);
+inheritPrototype(SubType, SuperType);  //对父类原型的复制，不会调用两次构造函数
 SubType.prototype.sayAge = function(){
     alert(this.age);
     }
+
+//class继承
+class A {}
+class B extends A 
+{
+  constructor() {   
+    super();
+  }
+}
+
+
+//object.setPrototypeOf()  
+//设置一个对象的__proto__属性，作用等同于__proto__
+//object.setPrototypeOf(obj,proto)
+
+//object.getPrototypeOf()  
+//获取一个对象的__proto__
