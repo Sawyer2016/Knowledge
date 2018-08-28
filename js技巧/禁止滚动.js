ebf15458d10b131493c5cc37e526950bf7ca3933
Sawyer2016 <szhou4@student.unimelb.edu.au>
2018-08-28 16:46:19 +0800

    update

--- js技巧/禁止滚动.js
+++ js技巧/禁止滚动.js
@@ -13,10 +13,12 @@ const disableScroll = () => {
   window.onmousewheel = stopEvent;
   window.addEventListener('touchmove', stopEvent, { passive: false }); // deal with preventDefault in mobile touchmove event
   window.onkeydown = keydown;
+  document.body.setAttribute('style', 'overflow-y:hidden'); //禁止底层body滚动
 };
 const enableScroll = () => {
   window.removeEventListener('DOMMouseScroll', stopEvent, false);
   window.onmousewheel = null;
   window.ontouchmove = null;
   window.onkeydown = null;
+  document.body.setAttribute('style', 'overflow-y:');
 };
